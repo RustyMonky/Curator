53ce9e6a8df76de4d766dc63d8807801081edd4b
Ian Cantor <icantor@fandm.edu>
2018-11-08 18:09:34 -0500

    Adds intial attack anim for demon, implements ray casting to detect player

--- entities/npc/demon/demon.gd
+++ entities/npc/demon/demon.gd
@@ -1,10 +1,14 @@
 extends KinematicBody2D
 
+enum STATE { ATTACK, CHASE }
+
 const SPEED = 64
 
 onready var animations = $demonAnimations
 onready var player = get_parent().get_node("player")
+onready var ray = $demonRay
 
+var currentState = STATE.CHASE
 var direction = Vector2()
 
 func _ready():
@@ -12,18 +16,37 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	if self.global_position.y < player.position.y:
-		self.direction.y = 1
-	elif self.global_position.y > player.position.y:
-		self.direction.y = -1
-	elif self.global_position.y == player.position.y:
-		self.direction.y = 0
+	if currentState == STATE.CHASE:
+		if self.global_position.y < player.position.y:
+			self.direction.y = 1
+		elif self.global_position.y > player.position.y:
+			self.direction.y = -1
+		elif self.global_position.y == player.position.y:
+			self.direction.y = 0
 
-	if animations.flip_h && player.position.x > self.position.x:
-		animations.flip_h = false
-		self.direction.x = 1
-	elif !animations.flip_h && player.position.x < self.position.x:
-		animations.flip_h = true
-		self.direction.x = -1
+		if animations.flip_h && player.position.x > self.position.x:
+			animations.flip_h = false
+			self.direction.x = 1
+		elif !animations.flip_h && player.position.x < self.position.x:
+			animations.flip_h = true
+			self.direction.x = -1
 
-	self.move_and_collide(self.direction.normalized() * SPEED * delta)
+		if animations.flip_h == true:
+			ray.rotation_degrees = 180
+		else:
+			ray.rotation_degrees = 0
+
+		self.move_and_collide(self.direction.normalized() * SPEED * delta)
+
+		if ray.is_colliding() && ray.get_collider() == player:
+			currentState = STATE.ATTACK
+			animations.set_animation("smash")
+			animations.play()
+
+# Signals
+
+func _on_demonAnimations_animation_finished():
+	if currentState == STATE.ATTACK:
+		currentState = STATE.CHASE
+		animations.set_animation("walkSide")
+		animations.play()
