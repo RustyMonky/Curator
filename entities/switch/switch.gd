67d91924c12ded787f4914f41370eee1d938079e
Ian Cantor <icantor@fandm.edu>
2018-11-29 17:43:16 -0500

    Adds background music -- still requires one for gameover

--- entities/switch/switch.gd
+++ entities/switch/switch.gd
@@ -13,6 +13,10 @@ func _ready():
 # Signals
 
 func _on_switchArea_body_entered(body):
+	# Only the player can activate switches!
+	if !body.is_in_group("player"):
+		return
+
 	match (currentState):
 		"Off":
 			currentState = "On"
--- levels/start/start.gd
+++ levels/start/start.gd
@@ -3,6 +3,8 @@ extends Control
 onready var painting = $painting
 onready var timer = $timer
 
+var preventInputs = false
+
 # When time permits, programatically generate the below array
 var paintingsArray = [
 	"res://assets/paintings/goya-satan-devouring-his-son.png",
@@ -16,12 +18,18 @@ var paintingIndex = 0
 
 func _ready():
 	painting.texture = load(paintingsArray[paintingIndex])
+
 	set_process(true)
 	set_process_input(true)
+
 	timer.start()
 
 func _input(event):
+	if preventInputs:
+		return
+
 	if event.is_action_pressed("ui_accept"):
+		preventInputs = true
 		fader.fadeToScene(levelLoader.getPath())
 
 func _process(delta):
