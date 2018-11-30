7dd16ea730f574abe3bb95430d89a11890766b99
Ian Cantor <icantor@fandm.edu>
2018-11-30 11:19:57 -0500

    Adds end game scene and resets game upon input

--- globals/levelLoader.gd
+++ globals/levelLoader.gd
@@ -29,5 +29,16 @@ func appendNextLevel():
 # getPath
 # Returns path of level scene based on current index
 func getPath():
+	if (currentIndex >= loadedLevels.size()):
+			return "res://levels/end/end.tscn"
+
 	var levelName = loadedLevels[currentIndex]
 	return "res://levels/" + levelName + "/" + levelName + ".tscn"
+
+# restart
+# Resets the loaded levels array
+func restart():
+	loadedLevels = []
+	currentIndex = 0
+	while loadedLevels.size() < levels.size():
+		appendNextLevel()
--- levels/end/end.gd
+++ levels/end/end.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var preventInputs = true
+var preventInputs = false
 
 func _ready():
 	set_process_input(true)
@@ -11,13 +11,7 @@ func _input(event):
 
 	if event.is_action_pressed("ui_accept"):
 		gameData.restart()
-		levelLoader.restart()
 
 	if event.is_action_released("ui_accept"):
 		fader.fadeToScene("res://levels/start/start.tscn")
 		preventInputs = true
-
-# Signals
-
-func _on_inputDelay_timeout():
-	preventInputs = false
